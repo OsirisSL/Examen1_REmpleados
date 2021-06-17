1e9582acc158a03432ddc2734e294cff93579eee
OsirisSL <sanabria1997.19@gmail.com>
2021-06-17 17:36:56 -0600

    Examen de Registro de Empleados

---
+++ Examen1_REmpleados/Examen1_REmpleados/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Examen1_REmpleados.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
